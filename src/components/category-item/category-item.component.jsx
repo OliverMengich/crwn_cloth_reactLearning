377ebc4856fca65370624543d6bf34239d896404
OliverMengich <kemeioliver2000@gmail.com>
2022-12-12 11:43:17 +0300

    Update

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,15 +1,11 @@
 import './category-item.styles.scss';
-const CategoryItem = ({category})=>{
-    const {imageUrl, title} = category;
-    return(
+const CategoryItem = ({category:{title,imageUrl}})=>(
         <div className="category-container">
-            {/* img */}
             <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
             <div className="category-body-container">
-              <h2>{title.toUpperCase()}</h2>
-              <p>Shop Now</p>
+                <h2>{title}</h2>
+                <p>Shop Now</p>
             </div>
-        </div>
-    )
-}
+		</div>
+);
 export default CategoryItem;

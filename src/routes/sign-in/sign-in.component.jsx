82455eac4d6e273e602129b99dd0e5ec66d0cbbd
OliverMengich <kemeioliver2000@gmail.com>
2022-12-13 09:22:20 +0300

    Appends signIn with Redirect and popup

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,32 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { signInWithGooglePopup,signInWithGoogleRedirect, createUserDocumentFromAuth, auth } from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
 // after user authenticates, store his information on firestore
 const SignIn = ()=>{
     const logGoogleUser = async ()=>{
         const {user} = await signInWithGooglePopup();
         // console.log(response);
         const userDocRef =await createUserDocumentFromAuth(user);
-
     }
+    useEffect(()=>{
+        async function fetchData(){
+            const res = await getRedirectResult(auth);
+            if(res){
+                const userDocRef = await createUserDocumentFromAuth(res.user);
+            }
+        }
+        fetchData();
+    },[])
+    // const logGoogleRedirectUser = async ()=>{
+    //     const {user} = await signInWithGoogleRedirect();
+    //     console.log(user);
+    //     // const userDocRef =await createUserDocumentFromAuth(user);
+    // }
     return(
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser} >SIgn in with GoogleAuthProvider</button>
+            <button onClick={signInWithGoogleRedirect} >SIgn in with GoogleRedirect</button>
         </div>
     )
 } 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -17,6 +17,7 @@ provider.setCustomParameters({
 });
 export const auth = getAuth();
 export const signInWithGooglePopup = ()=> signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = ()=> signInWithRedirect(auth,provider);
 export const db = getFirestore();
 export const createUserDocumentFromAuth = async(userAuth)=>{
     // store user who signed In

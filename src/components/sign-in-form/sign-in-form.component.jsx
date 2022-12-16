54b1be194be236faac7136e6778f6840763ee967
OliverMengich <kemeioliver2000@gmail.com>
2022-12-16 12:06:12 +0300

    AUthentification with ContextAPI

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -23,6 +23,7 @@ const SignInForm = ()=>{
     }
     const signInWithGoogle = async ()=>{
         const {user} = await signInWithGooglePopup();
+        setCurrentUser(user);
         await createUserDocumentFromAuth(user);
     }
     const handleFormSubmit = async (event)=>{
